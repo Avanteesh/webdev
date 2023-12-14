81db686ba6a5a30d7d81ce46cbb4f367aa0829f9
Avanteesh <143586258+Avanteesh@users.noreply.github.com>
2023-12-14 23:09:19 +0530

    Calculator a with a decent user interface!
    The calculator doesnt work though.. You can type numbers with those buttons onclick, but cant perform calculations. since Im just learning javascript I tried this project by myself just for fun. Since I already better learnt CSS, i thought now that Im learning basic javascript why not build some basic stuff!. it was a nice learning expireince! I look forward to to upgrade this soon

---
+++ calculator.js
@@ -0,0 +1,110 @@
+
+/* Display on click of input */
+
+// numbers
+
+function show_num_one() {
+    const v = document.getElementById("textar");
+    v.value += 1;
+}
+
+function show_num_two() {
+    const v1 = document.getElementById("textar");
+    v1.value += 2;
+}
+
+function show_num_three() {
+    const v2 = document.getElementById("textar");
+    v2.value += 3;
+}
+
+function show_num_four() {
+    const v3 = document.getElementById("textar");
+    v3.value += 4;
+}
+
+function show_num_five() {
+    const v4 = document.getElementById("textar");
+    v4.value += 5;
+}
+
+function show_num_six() {
+    const v5 = document.getElementById("textar");
+    v5.value += 6;
+}
+
+function show_num_seven()  {
+    const v6 = document.getElementById("textar");
+    v6.value += 7;
+}
+
+function show_num_eight() {
+    const v7 = document.getElementById("textar");
+    v7.value += 8;
+}
+
+function show_num_nine()  {
+    const v8 = document.getElementById("textar");
+    v8.value += 9;
+}
+
+function show_zero()  {
+    const v9 = document.getElementById("textar");
+    v9.value += 0;
+}
+
+// symbols 
+
+function show_plus()  {
+    const plus = document.getElementById("textar");
+    plus.value += "+";
+}
+
+function show_minus()  {
+    const minus = document.getElementById("textar");
+    minus.value += "-";
+}
+
+function show_divide()  {
+    const divide = document.getElementById("textar");
+    divide.value += "/";
+}
+
+function show_multiply()  {
+    const mult = document.getElementById("textar");
+    mult.value += "x";
+}
+
+function show_power()  {
+    const pow = document.getElementById("textar");
+    pow.value += "^";
+}
+
+/* Clear */
+function clear_all() {
+    const clear = document.getElementById("textar");
+    clear.value = " ";
+}
+
+/* Display the value */
+
+/* function value()  {
+    const val = document.getElementById("textar");
+    val.value = eval();
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
